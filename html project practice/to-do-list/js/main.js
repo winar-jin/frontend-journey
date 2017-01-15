55590beec1522bfde17cf8dd6acfda8bfd388910
winar-jin <winar1994@outlook.com>
2017-01-15 12:22:49 +0800

    to do list

---
+++ html project practice/to-do-list/js/main.js
@@ -0,0 +1,25 @@
+let addItemButton = document.querySelector('header button[id="addItem"]');
+addItemButton.addEventListener('click',addtodoItem);
+
+
+
+
+// addtodoItem
+function addtodoItem(e){
+    let item = document.querySelector('header input').value;
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
